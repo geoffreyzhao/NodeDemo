ba02be515245436a0e72ee3793a31db69d7f8195
= <Geoffrey.Zhao@t04300-225.augmentum.com.cn>
2014-06-27 14:13:56 +0800

    add http js

--- http.js
+++ http.js
@@ -1,7 +1,19 @@
 var http = require('http');
-http.createServer(function(req, res){
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write('<head><meta charset="utf-8" /></head>');
-	res.end('你好, NodeJs \n');
-}).listen(80, "127.0.0.1");
-console.log('server running on http://127.0.0.1:80');
+var server = http.createServer(function(req, res){
+	// res.writeHead(200, {'Content-Type': 'text/html'});
+	// res.write('<head><meta charset="utf-8" /></head>');
+	// res.end('你好, NodeJs \n');
+});
+server.listen(3306, '127.0.0.1');
+server.on('listening', function() {
+	console.log('server is listening');
+	server.close();
+});
+server.on('close', function(){
+	console.log('server close');
+});
+server.on('error', function(e) {
+	if (e.code == 'EADDRINUSE') {
+		console.log('server port is unused');
+	}
+});
